d47050c28f99c9c0bea6a7a3d8371105f8ee96d2
richmond-andoh <richmondandoh00@gmail.com>
2024-05-17 21:36:43 +0000

    Modifies each string by capitalizing it if the corresponding number is even and converting it to lowercase if the number is odd.

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,3 +1,12 @@
 const processArray = (arrOfNum) => {
    return arrOfNum.map(num => (num % 2 === 0 ? num ** 2 : num * 3));
 };
+
+
+
+const formatArrayStrings = (arrOfStr, arrOfNum) => {
+    return arrOfStr.map((str, indx) => {
+        const num = arrOfNum[indx];
+        return num % 2 === 0 ? str.toUpperCase : str.toLowerCase;
+    });
+};
